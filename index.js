ee0475b6f790350538c5e4136ca802c1eebc9f42
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-08-05 10:15:27 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import authComponentData from './data/auth-component.json';
 import bankComponentData from './data/bank-component.json';
 
-module.exports = {
-  authComponentData,
-  bankComponentData,
+module.exports.getComponentData = (id) => {
+  switch (id) {
+    case '72520fc5-6be5-4ee5-b986-0e688ab4adff':
+      return authComponentData;
+    case 'fc1456e9-5a30-4d02-bed5-8fc3a3367ab3':
+      return bankComponentData;
+    default:
+      break;
+  }
 };
