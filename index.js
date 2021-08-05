bae554e17d79c64d0609b06150c2527b411060a8
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-08-05 11:10:15 +0700

    update theme component data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const authComponentData = require('./data/auth-component.json');
 const bankComponentData = require('./data/bank-component.json');
+const themeComponentData = require('./data/theme-component.json');
 
 exports.getDataByComponentId = (id) => {
   switch (id) {
@@ -7,6 +8,8 @@ exports.getDataByComponentId = (id) => {
       return authComponentData;
     case 'fc1456e9-5a30-4d02-bed5-8fc3a3367ab3':
       return bankComponentData;
+    case 'd4cc3288-fa4a-4fd7-8ae2-cdd26a06d215':
+      return themeComponentData;
     default:
       break;
   }
