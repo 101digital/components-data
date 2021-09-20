fc07f6f3e5806d04cbf18189e89880a84b53e495
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-09-20 16:04:19 +0700

    update data

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const authComponentData = require('./data/auth-component.json');
-const bankComponentData = require('./data/bank-component.json');
+const walletComponentData = require('./data/wallet-component-data.json');
+const transactionComponentData = require('./data/transaction-component-data.json');
+const accountLinkingComponentData = require('./data/account-linking-component-data.json');
 const themeComponentData = require('./data/theme-component.json');
 const productComparionData = require('./data/product-comparison-component.json');
 
 exports.getDataByComponentId = (id) => {
   switch (id) {
-    case '72520fc5-6be5-4ee5-b986-0e688ab4adff':
+    case 'auth-component':
       return authComponentData;
-    case 'fc1456e9-5a30-4d02-bed5-8fc3a3367ab3':
-      return bankComponentData;
-    case '74a603bd-a36a-4f2b-bc18-385a4101878c':
+    case 'wallet-component':
+      return walletComponentData;
+    case 'transaction-component':
+      return transactionComponentData;
+    case 'account-linking-component':
+      return accountLinkingComponentData;
+    case 'theme-component':
       return themeComponentData;
-    case 'fddeac89-dae0-40ed-913b-afe990e5c1d7':
+    case 'product-comparison-component':
       return productComparionData;
     default:
       break;
