4cfe523c38bae432b1b06f278141126572dce8c5
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2022-01-21 16:58:43 +0700

    gitpkg

---
+++ index.js
@@ -0,0 +1,31 @@
+const authComponentData = require('./data/auth-component.json');
+const walletComponentData = require('./data/wallet-component-data.json');
+const transactionComponentData = require('./data/transaction-component-data.json');
+const accountLinkingComponentData = require('./data/account-linking-component-data.json');
+const themeComponentData = require('./data/theme-component.json');
+const productComparionData = require('./data/product-comparison-component.json');
+const customerComponentData = require('./data/customer-component-data.json');
+const invoiceComponentData = require('./data/invoice-component-data.json')
+
+exports.getDataByComponentId = (id) => {
+  switch (id) {
+    case 'auth-component':
+      return authComponentData;
+    case 'wallet-component':
+      return walletComponentData;
+    case 'transaction-component':
+      return transactionComponentData;
+    case 'account-linking-component':
+      return accountLinkingComponentData;
+    case 'theme-component':
+      return themeComponentData;
+    case 'product-comparison-component':
+      return productComparionData;
+    case 'customer-component':
+      return customerComponentData;
+    case 'invoice-component':
+      return invoiceComponentData;
+    default:
+      break;
+  }
+};
